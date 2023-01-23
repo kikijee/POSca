011492c95b34fba50e08f4f35c7579fa5f2b804b
Christian Manibusan <cam007mani@gmail.com>
2023-01-23 13:56:37 -0800

    added initial sql data table creation code.

---
+++ POS/SQL/QuickDBD-export.sql
@@ -0,0 +1,36 @@
+﻿CREATE TABLE `Users` (
+    `Username` varchar(20)  NOT NULL ,
+    `Password` varchar(20)  NOT NULL ,
+    `Admin` boolean  NOT NULL ,
+    PRIMARY KEY (
+        `Username`
+    )
+);
+
+CREATE TABLE `Items` (
+    `ItemID` int  NOT NULL ,
+    `Name` String  NOT NULL ,
+    `Price` decimal(,2)  NOT NULL ,
+    PRIMARY KEY (
+        `ItemID`
+    )
+);
+
+CREATE TABLE `Orders` (
+    `OrderID` int  NOT NULL ,
+    `Subtotal` decimal(,2)  NOT NULL ,
+    `Total` decimal(,2)  NOT NULL ,
+    `Time` String  NOT NULL ,
+    `Server` String  NOT NULL ,
+    `Paid` boolean  NOT NULL ,
+    `PayType` String  NULL ,
+    `Change` decimal(,2)  NULL ,
+    `NumItems` int  NOT NULL ,
+    PRIMARY KEY (
+        `OrderID`
+    )
+);
+
+ALTER TABLE `Orders` ADD CONSTRAINT `fk_Orders_Server` FOREIGN KEY(`Server`)
+REFERENCES `Users` (`Username`);
+
