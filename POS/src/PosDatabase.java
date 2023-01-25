ad7b5002338da0440dfdbb99e57b50e1c28f2e5e
Christian Manibusan <cam007mani@gmail.com>
2023-01-24 16:44:32 -0800

    additional user table query functions

--- POS/src/PosDatabase.java
+++ POS/src/PosDatabase.java
@@ -57,8 +57,8 @@ public class PosDatabase {
 		return null;
     }
     
-    // have not tested yet
-    public boolean add_user(String user, String password) {
+    // HAS NOT BEEN TESTED YET
+    public boolean add_user(String user, String password, boolean admin) {
     	try {
 			Class.forName(myDriver);
 			Connection con = DriverManager.getConnection(myUrl,myUsername,myPassword);
@@ -67,6 +67,7 @@ public class PosDatabase {
 			ResultSet rs = ps.executeQuery(query);	// result set containing the rows collected from query
 			ps.setString(1,user);
 			ps.setString(2, query);
+			ps.setBoolean(3, admin);
 			int num = ps.executeUpdate();
 			if(num!=1) {ps.close();con.close();return false;}	// this should not happen
 			ps.close();
@@ -76,6 +77,49 @@ public class PosDatabase {
 			e1.printStackTrace();
 		}
     	System.out.println("1 row inserted into USERS");
+    	return true;
+    }
+    
+    // HAS NOT BEEN TESTED YET
+    public boolean change_privilege(String username, boolean status) {
+    	try {
+			Class.forName(myDriver);
+			Connection con = DriverManager.getConnection(myUrl,myUsername,myPassword);
+			String query = "UPDATE users SET admin=? WHERE username=?";
+			PreparedStatement ps = con.prepareStatement(query);	// statement object for String to SQL
+			ps.setBoolean(1, status);
+			ps.setString(2, username);
+			ResultSet rs = ps.executeQuery(query);	// result set containing the rows collected from query
+			int num = ps.executeUpdate();
+			if(num == 0) {ps.close();con.close();return false;}	// user does not exist
+			ps.close();
+	    	con.close();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	System.out.println("1 row updated into USERS");
+    	return true;
+    }
+    
+    // HAS NOT BEEN TESTED YET
+    public boolean delete_user(String username) {
+    	try {
+			Class.forName(myDriver);
+			Connection con = DriverManager.getConnection(myUrl,myUsername,myPassword);
+			String query = "DELETE FROM users WHERE username=?";
+			PreparedStatement ps = con.prepareStatement(query);	// statement object for String to SQL
+			ps.setString(1, username);
+			ResultSet rs = ps.executeQuery(query);	// result set containing the rows collected from query
+			int num = ps.executeUpdate();
+			if(num == 0) {ps.close();con.close();return false;}	// invalid username
+			ps.close();
+	    	con.close();
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	System.out.println("1 row deleted from USERS");
     	return true;
     }
 }
